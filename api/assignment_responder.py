eef59c76d001fd4bf14d042e1f428be9be4cd48d
Rob Svirskas <svirskasr@janelia.hhmi.org>
2019-10-08 16:20:15 -0400

    Changed proofreader display. Changed protocol/proofreader select to search boxes. Modified peroject selection.

--- api/assignment_responder.py
+++ api/assignment_responder.py
@@ -114,7 +114,7 @@ class CustomJSONEncoder(JSONEncoder):
             return list(iterable)
         return JSONEncoder.default(self, obj)
 
-__version__ = '0.8.7'
+__version__ = '0.8.8'
 app = Flask(__name__, template_folder='templates')
 app.json_encoder = CustomJSONEncoder
 app.config.from_pyfile("config.cfg")
@@ -688,21 +688,28 @@ def get_unassigned_project_tasks(ipd, project_id, num_tasks):
     return tasks
 
 
-def project_summary_date(start, stop):
-    ''' Build a project summary date query
+def project_summary_query(protocol, start, stop):
+    ''' Build a project summary query
         Keyword arguments:
           start: start date
           stop: stop date
+          protocol: protocol
         Returns:
           SQL query
     '''
     sql = READ['PSUMMARY']
-    if (start or stop):
-        clause = []
-        if start:
-            clause.append(" DATE(p.create_date) >= '%s'" % start)
-        if stop:
-            clause.append(" DATE(p.create_date) <= '%s'" % stop)
+    clause = []
+    protocols = []
+    if protocol:
+        for pro in protocol.split(','):
+            protocols.append("'" + pro + "'")
+        protocol_str = ','.join(protocols)
+        clause.append(" t.protocol IN (%s)" % protocol_str)
+    if start:
+        clause.append(" DATE(p.create_date) >= '%s'" % start)
+    if stop:
+        clause.append(" DATE(p.create_date) <= '%s'" % stop)
+    if clause:
         where = ' AND '.join(clause)
         sql = sql.replace('GROUP BY', 'WHERE ' + where + ' GROUP BY')
     return sql
@@ -1679,9 +1686,10 @@ def download(fname):
 
 
 @app.route('/projectlist')
-@app.route('/projectlist/<start>')
-@app.route('/projectlist/<start>/<stop>')
-def show_projects(start='', stop=''): # pylint: disable=R0914
+@app.route('/projectlist/<protocol>')
+@app.route('/projectlist/<protocol>/<start>')
+@app.route('/projectlist/<protocol>/<start>/<stop>')
+def show_projects(protocol='', start='', stop=''): # pylint: disable=R0914
     ''' Projects
     '''
     if not request.cookies.get(app.config['TOKEN']) or not request.cookies.get('flyem-services'):
@@ -1690,7 +1698,7 @@ def show_projects(start='', stop=''): # pylint: disable=R0914
     user, face = get_web_profile()
     permissions = check_permission(user)
     try:
-        g.c.execute(project_summary_date(start, stop))
+        g.c.execute(project_summary_query(protocol, start, stop))
         rows = g.c.fetchall()
     except Exception as err:
         return render_template('error.html', urlroot=request.url_root,
@@ -1709,7 +1717,6 @@ def show_projects(start='', stop=''): # pylint: disable=R0914
                    + ''.join('<td style="text-align: center">%s</td>')*5 + "</tr>"
         fileoutput = ''
         ftemplate = "\t".join(["%s"]*8) + "\n"
-        protocols = dict()
         for row in rows:
             if 'admin' not in permissions and row['protocol'] not in permissions:
                 continue
@@ -1718,7 +1725,6 @@ def show_projects(start='', stop=''): # pylint: disable=R0914
                 rclass = 'open'
             name = re.sub('[^0-9a-zA-Z]+', '_', row['protocol'])
             rclass += ' ' + name
-            protocols[name] = row['protocol']
             proj = '<a href="/project/%s">%s</a>' % (row['project'], row['project'])
             active = "<span style='color:%s'>%s</span>" \
                      % (('lime', 'YES') if row['active'] else ('red', 'NO'))
@@ -1734,11 +1740,16 @@ def show_projects(start='', stop=''): # pylint: disable=R0914
                    % (downloadable) + 'role="button">Download table</a>' + projects
     else:
         projects = "There are no projects"
+    result = initialize_result()
+    execute_sql(result, "SELECT cv_term FROM cv_term_vw WHERE cv='protocol' ORDER BY 1", 'temp')
+    selected = protocol.split(',') if protocol else []
+    for row in result['temp']:
+        if bool(row['cv_term'] in sys.modules):
+            sel = 'SELECTED' if (not selected or row['cv_term'] in selected) else ''
+            protocols += '<option value="%s" %s>%s</option>' % (row['cv_term'], sel, row['cv_term'])
     if check_permission(user, 'admin'):
-        result = initialize_result()
-        execute_sql(result, "SELECT cv_term FROM cv_term_vw WHERE cv='protocol' ORDER BY 1", 'temp')
         newproject = '''
-        <select id="protocol" onchange='new_project(this);'>
+        <select id="pprotocol" onchange='new_project(this);'>
         <option value="all" SELECTED>Select a protocol for a new project...</a>
         '''
         for row in result['temp']:
