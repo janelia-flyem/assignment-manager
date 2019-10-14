6c79c30c68b3fb457fb2ad59cb41fb4f46b9c321
Rob Svirskas <svirskasr@janelia.hhmi.org>
2019-10-14 15:27:55 -0400

    Added restart_production.sh

---
+++ restart_production.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker-compose -f docker-compose-prod.yml down
+docker image ls | grep registry.int.janelia.org | awk '{print $3}' | xargs docker image r
+docker volume rm assignment-manager_static_volume
+docker pull registry.int.janelia.org/flyem/assignment-manager
+docker-compose -f docker-compose-prod.yml up
